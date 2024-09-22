976e6fbe52bb7ba4977421685c5801248d601488
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-09-22 12:48:12 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-# from cnnClassifier import logger
 from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME = "Data_Ingestion_Stage"
 
-logger.info("Welcome to cnnClassifier")
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
